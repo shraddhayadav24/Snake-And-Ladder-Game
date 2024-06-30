f5d1fc2ebbbdf1bd899e32607a1d1439bfd1f8d5
shraddhayadav24 <slyadav240901@gmail.com>
2024-06-30 21:23:16 +0530

    UC2 Added

--- src/com/ladder/SnakeLadder.java
+++ src/com/ladder/SnakeLadder.java
@@ -2,14 +2,14 @@ package com.ladder;
 
 public class SnakeLadder {
 	public static void main(String[] args) {
-		int diceRoll = 0;
-        int current_position = 0;
-        int max_position = 100;
+		 int diceRoll = 0;
+	        int current_position = 0;
+	        int max_position = 100;
 
-        System.out.println("Welcome to Snake and Ladder Game!");
-
-        System.out.println("Start position :" + current_position);
-
+	        System.out.println("Welcome to Snake and Ladder Game!");
+	        System.out.println("Current position :" + current_position);
+	        diceRoll = (int) ((Math.random() * 6) + 1);
+	        System.out.println("Dice Number :" + diceRoll);
         
 }
 }
